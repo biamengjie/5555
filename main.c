6417e489e871ecfa03cad0fcbe3e4b91ce4760a7
Your Name <you@example.com>
2019-01-17 08:52:15 +0800

    添加一个敏感字符

--- main.c
+++ main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
-
+char arr[1000];
+int index=0;
+int isMiongan(char x)
 int main(){
 
-    printf("hello world!");
+    printf("jinruminganzifuguanlixitong:\n");
+    while(1){
+        printf("tianjiayigeminganzifu:n\");
+        printf("shanchutigeminganzifu:\n");
+        printf("xianshisouyoudeminganzifu:\n");
+        printf("tihuanminganzifu:\n");
+        printf("chengxutuichu;\n");
+        printf("qingshuruxiangyingdeminganzifu:\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1){
+            printf("tianjiayigeminganzifu:\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index]=m;
+            index++;
+            printf("tianjiachnegong,dianjihuichejixu:\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+
+            
+
+
+
+        }
+        if(code==2){
+           
+
+            
+        }
+        if(code==3){
+            
+        }
+        if(code==4){
+            
+        }
+        if(code==5){
+            
+        }
+    }
 
     return 0;
 
